d15a4b10814ee9980a4c8fa80c35ca51a6930b5a
thrzl <thrizzle@proton.me>
2026-07-10 10:55:10 -0400

    fix: encode structs to json (badly)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import Any
 from fastmcp import FastMCP
 from otx_py import OTXClient
 from dotenv import load_dotenv
 import os
+from msgspec import json
 
 load_dotenv()
 
@@ -10,15 +12,17 @@ otx = OTXClient(os.environ["OTX_KEY"])
 
 
 @app.tool(description="Get the latest pulses from OTX")
-def pulses() -> dict:
+def pulses() -> dict[str, dict]:
     pulses = otx.pulses(5)
-    return {"pulses": pulses}
+    print(json.decode(json.encode(pulses)))
+    return {"pulses": json.decode(json.encode(pulses))}
 
 
 @app.tool(description="Get a specific pulse from OTX by its ID")
-def get_pulse(pulse_id: str) -> dict:
+def get_pulse(pulse_id: str) -> dict[str, Any]:
     pulse = otx.pulse_id(pulse_id)
-    return {"pulse": pulse}
+    print(json.decode(json.encode(pulse)))
+    return {"pulse": json.decode(json.encode(pulse))}
 
 
 if __name__ == "__main__":
