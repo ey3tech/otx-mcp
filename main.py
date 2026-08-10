f8fb63b29f6a0bcfc52ac1b026813a6ff68b75f5
4kadinspencer-commits <4kadinspencer@gmail.com>
2026-08-10 15:14:48 +0000

    Add arguement to tools

--- main.py
+++ main.py
@@ -11,39 +11,50 @@ otx = OTXClient(os.environ["OTX_KEY"])
 
 
 @app.tool(description="Get the latest pulses from OTX")
-def get_pulses() -> dict[str, dict]:
-    pulses = otx.get_pulses(5)
+def get_pulses(limit: int = 5) -> dict[str, dict]:
+    pulses = otx.get_pulses(limit)
     return {"pulses": to_builtins(pulses)}
+
 
 @app.tool(description="Get different indicator types")
 def get_indicator_types() -> dict[str, dict]:
     types = otx.get_indicator_types()
     return {"indicator_types": to_builtins(types)}
 
+
 @app.tool(description="Search for users")
-def search_user(query: str) -> dict[str, dict]:
-    users = otx.search_users(query, 1, 20)
+def search_user(
+    query: str,
+    page: int = 1,
+    limit: int = 20,
+) -> dict[str, dict]:
+    users = otx.search_users(query, page, limit)
     return {"search_users": to_builtins(users)}
 
+
 @app.tool(description="Search for pulses")
-def search_pulses(query: str) -> dict[str, dict]:
-    pulses = otx.search_pulses(query, 1, 20)
+def search_pulses(query: str, page: int = 1, limit: int = 20) -> dict[str, dict]:
+    pulses = otx.search_pulses(query, page, limit)
     return {"pulses": to_builtins(pulses)}
+
 
 @app.tool(description="Get user's username and detail")
 def get_user(username: str, detail: bool) -> dict[str, dict]:
     user = otx.get_user(username, detail)
     return {"pulses": to_builtins(user)}
 
+
 @app.tool(description="Get a specific pulse from OTX by its ID")
 def get_pulse(pulse_id: str) -> dict[str, dict]:
     pulse = otx.get_pulse_by_id(pulse_id)
     return {"pulses": to_builtins(pulse)}
 
+
 @app.tool(description="Get indicators")
-def get_pulse_indicators(pulse_id: str, limit: int=25) -> dict[str, dict]:
+def get_pulse_indicators(pulse_id: str, limit: int = 25) -> dict[str, dict]:
     pulse_indicator = otx.get_pulse_indicators(pulse_id, limit)
     return {"indicators": to_builtins(pulse_indicator)}
+
 
 if __name__ == "__main__":
     app.run(transport="http")
