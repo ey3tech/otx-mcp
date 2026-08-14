4c7a18627c2ec284827f5308ea16aba16f42f7a3
4kadinspencer-commits <4kadinspencer@gmail.com>
2026-08-14 15:23:06 +0000

    Completed test suite
    Co-authored-by: te <thrizzle@proton.me>

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,8 +1,6 @@
-from mcp.types import TextContent
 import pytest
 from fastmcp import Client
 from main import app
-from msgspec import json
 
 
 @pytest.fixture
@@ -54,7 +52,7 @@ async def test_search_user(client: Client, query: str, expected_id: int):
 )
 async def test_search_pulses(client: Client, query: str, expected_id: str):
     result = await client.call_tool(
-        "search_pulses", {"query": query, "page": 1, "limit": 1})
+        "search_pulses", {"query": query, "page": 1, "limit": 20})
     
     json_data = result.structured_content
 
@@ -66,4 +64,132 @@ async def test_search_pulses(client: Client, query: str, expected_id: str):
 
     assert pulse_exists, f"expected to find pulse with ID {expected_id}"
 
+@pytest.mark.parametrize(
+    ["username", "expected_id"],
+    [("twofortythree", 413902), ("MST478293", 402211), ("cyberhunterautofeed", 182496)],
+)
+async def test_get_user(client: Client, username: str, expected_id: str):
+    result = await client.call_tool(
+        "get_user", {"username": username, "detail": False})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    user_is_correct = json_data["pulses"]["user_id"] == expected_id
+
+    assert user_is_correct, f"expected to find user {expected_id} but got user {json_data['pulses']['user_id']}"
+
+@pytest.mark.parametrize(
+    ["expected_name", "expected_id"],
+    [("PoS Scammers Toolbox", "546ce8eb11d40838dc6e43f1"), ("RAZOR BLADES IN THE CANDY JAR", "546cf5ba11d40839ea8821ca"), ("Operation Double Tap", "546fc7bf11d4083bc021c37f")],
+)
+async def test_get_pulse(client: Client, expected_name: str, expected_id: str):
+    result = await client.call_tool(
+        "get_pulse", {"pulse_id": expected_id})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    pulse_exists = json_data["pulses"]["name"].strip() == expected_name.strip()
+
+    assert pulse_exists, f"expected to find specific pulse from OTX with ID {expected_name}. instead found {json_data['pulses']['name']}"
+
+@pytest.mark.parametrize(
+    ["expected_name", "expected_id"],
+    [("PoS Scammers Toolbox", "546ce8eb11d40838dc6e43f1"), ("RAZOR BLADES IN THE CANDY JAR", "546cf5ba11d40839ea8821ca"), ("Operation Double Tap", "546fc7bf11d4083bc021c37f")],
+)
+async def test_get_pulse_indicators(client: Client, expected_name: str, expected_id: str):
+    result = await client.call_tool(
+        "get_pulse_indicators", {"pulse_id": expected_id, "limit": 10})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    assert len(json_data["indicators"]["results"]) >= 1
+
+    # indicator_exists = any(indicator["id"] == expected_name for indicator in json_data["indicators"]["results"])
+
+    # assert indicator_exists, f"expected to find indicators for indicator {expected_id}"
+
+
+@pytest.mark.parametrize(
+    "query",
+    ["cjds9)MFJ9jfm2489jfdsajmf89sajdfo", "fka90fm34iamf0fj0934imfdisaojf", "fdasm89fjm389fjmasd0f8,jf0j348mf,d8safi"],
+)
+async def test_search_user_fail(client: Client, query: str):
+    result = await client.call_tool(
+        "search_user", {"query": query, "page": 1, "limit": 5}
+    )
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    search_user_exists = any(user["name"] == query for user in json_data["search_users"]["results"])
+
+    assert not search_user_exists, f"expected to not find user with name {query}"
+
+@pytest.mark.parametrize(
+    "query",
+    ["PoS Scammers Toolbox", "RAZOR BLADES IN THE CANDY JAR", "Operation Double Tap"],
+)
+async def test_search_pulses_fail(client: Client, query: str):
+    result = await client.call_tool(
+        "search_pulses", {"query": query, "page": 1, "limit": 5})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    pulse_exists = any(pulse["name"].strip() == query for pulse in json_data["pulses"]["results"])
+
+    assert pulse_exists, f"expected to find pulse with ID {query}"
+
+@pytest.mark.parametrize(
+    "username",
+    ["ddejhgrdiunuirvn", "feijfinvj", "fneionorinr"],
+)
+async def test_get_user_fail(client: Client, username: str):
+    result = await client.call_tool(
+        "get_user", {"username": username, "detail": False})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    user_is_correct = json_data["pulses"]["user_id"] == username
+
+    assert user_is_correct, f"expected to find user {username} but got user {json_data['pulses']['user_id']}"
+
+@pytest.mark.parametrize(
+    "expected_id",
+    ["a"*24, "b"*24, "c"*24],
+)
+async def test_get_pulse_fail(client: Client, expected_id: str):
+    result = await client.call_tool(
+        "get_pulse", {"pulse_id": expected_id})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    pulse_exists = json_data["pulses"].get("id") != expected_id
+
+    assert pulse_exists, f"expected not to find specific pulse from OTX with ID {expected_id}. instead found {json_data['pulses']['id']}"
+
+@pytest.mark.parametrize(
+    "expected_id",
+    ["a"*24, "b"*24, "c"*24],
+)
+async def test_get_pulse_indicators_fail(client: Client, expected_id: str):
+    result = await client.call_tool(
+        "get_pulse_indicators", {"pulse_id": expected_id, "limit": 10})
+    
+    json_data = result.structured_content
+
+    assert json_data is not None
+
+    assert not json_data["indicators"].get("results"), f"expected to find indicators for indicator {expected_id}"
 
